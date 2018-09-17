b7e0b22291c03a91dd360791dd775c80dc37ae57
essec <jakee.indapanya@gmail.com>
2018-09-17 10:14:29 +0700

    create react app & add CharacterCard.js

---
+++ card-game/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <CharacterCard value='h'/>
+        <CharacterCard value='i'/>
+      </div>
+    );
+  }
+}
+
+export default App;
