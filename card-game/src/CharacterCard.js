dd920025c4d1aec150ec7701d84ddeb687709196
essec <jakee.indapanya@gmail.com>
2018-09-21 16:05:33 +0700

    change guess type

--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -13,20 +13,27 @@ export default class CharacterCard extends Component{
         this.setState({active: false})
        }
     }
-    
+
     activate = ()=>{
         if(!this.state.active){
             this.props.activationHandler(this.props.value)
             this.setState({active: true})
+            if(this.state.completed == false){
+                
+            }
         }
     }
 
     render(){
         let className = `card ${this.state.active ? 'activeCard': ''}`
+        // console.log(this.props.attempt)
+
         return(
-            <div className={className} onClick={this.activate}>
-                {this.props.value}
-            </div>
+                <div className={className} onClick={this.activate}>
+                   {this.props.value}
+                   
+                </div>
+           
         );
     }
 }
--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -10,7 +10,8 @@ const prepareStateFromWord = (given_word) => {
         word,
         chars,
         attempt: 1,
-        guess: [],
+        left_attempt: 5,
+        guess: "",
         completed: false
     }
 }
@@ -22,22 +23,42 @@ Component{
     }
 
     activationHandler = (c) => {
-        let guess = [this.state.guess, c]
+        // console.log(`${c}`)
+        let guess = "" + this.state.guess + c
+        
         this.setState({guess})
         if(guess.length == this.state.chars.length){
-            if(guess.join('').toString() == this.state.word){
-            this.setState({guess: [], completed: true})
+            if(guess == this.state.word){
+            this.setState({guess: "", completed: true})
             }
             else{
-            this.setState({guess: [], attempt: this.state.attempt + 1})
+            this.setState({guess: "", attempt: this.state.attempt + 1, left_attempt: this.state.left_attempt - 1})
+
             }
         }
+        // console.log(this.state)
+        console.log("guess: "+ guess)
+        console.log("word : "+ this.state.word  )
+        console.log("status: " + this.state.completed)
     }
+
     
     render() {
         return(
             <div>
-                {Array.from(this.props.value).map((c, i)=> <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>)}
+                <div>
+                {Array.from(this.state.chars).map((c, i)=> <CharacterCard value={c} 
+                                                            key={i} 
+                                                            activationHandler={this.activationHandler} 
+                                                            attempt={this.state.attempt} 
+                                                            left_attempt={this.state.left_attempt}
+                                                            completed={this.state.completed}/>)}
+                </div>
+                <div className="score">
+                attempt = {this.state.attempt}
+                <br></br>
+                your attempt left = {this.state.left_attempt}
+                </div>
             </div>
         );
     }
